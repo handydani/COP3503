22424aa153c075165c6ced8b7a7b9e231b2406e4
Daniela Isabel Travieso <dtravie@gmail.com>
2017-02-21 15:58:07 -0500

    added header and cpp files

---
+++ pa1.h
@@ -0,0 +1,19 @@
+//
+//  header.h
+//  Magic Square
+//
+//  Created by Daniela Isabel Travieso on 2/12/17.
+//  Copyright © 2017 Daniela Isabel Travieso. All rights reserved.
+//
+
+#ifndef header_h
+#define header_h
+
+
+//Daniela Isabel Travieso
+//COP 3503
+//Section: 17H4
+//Description:
+//This is programming assignment #1 where we were asked to create 4 magic squares and print the sums of rows, columns, and diagonals
+
+#endif /* header_h */
