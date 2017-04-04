b9c9181826c7c59925ec27bf90ef4c90920a5dd9
Daniela Isabel Travieso <dtravie@gmail.com>
2017-04-04 16:10:15 -0400

    added what I got ahead on today

--- pa3/pa3/pa3.cpp
+++ pa3/pa3/pa3.cpp
@@ -10,12 +10,60 @@
 #include "pa3.h"
 #include <fstream>
 #include <string>
+#include <stack>
 
-
-int main(int argc, const char * argv[])
+int main()
 {
 
+  std::string fileName = "";
+  std::cout << "Please enter the name of the input file: " << std::endl;
+  std::cin >> fileName;
+  std::ifstream file(fileName);
+
+  /**********************************/
+
+  if(!file)
+  {
+    std::cout << "Something went wrong..." << std::endl;
+    return 1;
+  }
+
+  std::cout << "Success! Proceeding... " << std::endl;
+
+  /**********************************/
+
+  // std::stack <char> s;
+  // char token();
+  // char bitch();
+  // while( file.good() )
+  // {
+	// 	file >> token;
+	// 	s.push(token);
+	// }
+  //
+  // for(int i = 0; i < s.size(); ++i)
+  // {
+  //   bitch = s.top();
+  //   std::cout << bitch << std::endl;
+  //   s.pop();
+  // }
+
+  //find depth
+
+  //keywords:
+  //identifiers:
+  //constant:
+  //operators:
+  //delimiter:
+
+  //syntax errors:
+  //close file
+  /**********************************/
 
 
-    return 0;
+  file.close();
+  return 0;
 }
+// StackClass readFile(){
+//
+//}
--- pa3/pa3/pa3.h
+++ pa3/pa3/pa3.h
@@ -8,6 +8,83 @@
 
 #ifndef Header_h
 #define Header_h
+#include <iostream>
+#include <string>
+
+class StackClass
+{
+
+  private:
+
+  public:
+    StackClass();
+    void push(std::string token);
+    std::string pop();
+    bool isEmpty();
+    bool contains(string data);
+    int size();
+    void printStack();
+    bool exists();
+    string top();
+    int size;
+
+    typedef struct Node{
+      std:string info;
+      Node *next;
+      bool isFree; //edit later
+    }* nodePointer;
+
+    nodePointer head;
+    nodePointer curr;
+    nodePointer temp;
+
+
+}
+
+StackClass readFile(std::string name);
+
+Stack::Stack()
+{
+  head = NULL;
+  curr = NULL;
+  temp = NULL;
+  size = 0;
+}
+void StackClass::push(std::string token)
+{
+    nodePointer nPtr = new Node;
+    n->next = NULL;
+    n->info = token;
+
+    if(head != NULL)
+    {
+        curr = head;
+
+        while(curr->next!=NULL)
+        {
+            curr = curr->next;
+        }
+
+        curr->next = n;
+    }
+    else
+    {
+        head = n;
+    }
+    size++;
+}
+std::string StackClass::pop()
+{
+    std::string popped;
+    curr = head;
+
+    
+
+
+    size--;
+}
+
+
 
 
 #endif /* Header_h */
